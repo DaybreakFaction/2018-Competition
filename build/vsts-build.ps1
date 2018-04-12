c65e7c4e8c1fbebb21eb34456f779294dc7e462a
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2018-04-12 09:38:33 -0700

    Default template

---
+++ build/vsts-build.ps1
@@ -0,0 +1,13 @@
+<#
+This script publishes the module to the gallery.
+It expects as input an ApiKey authorized to publish the module.
+
+Insert any build steps you may need to take before publishing it here.
+#>
+param (
+	$ApiKey,
+	$WhatIf
+)
+
+if ($WhatIf) { Publish-Module -Path "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)\Daybreak2018" -NuGetApiKey $ApiKey -Force -WhatIf }
+else { Publish-Module -Path "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)\Daybreak2018" -NuGetApiKey $ApiKey -Force }
