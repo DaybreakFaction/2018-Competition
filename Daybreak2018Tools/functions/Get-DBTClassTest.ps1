ec146689b73fc724ea1c843325e710abda22f2f7
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2018-04-12 14:17:42 -0700

    updates

---
+++ Daybreak2018Tools/functions/Get-DBTClassTest.ps1
@@ -0,0 +1,66 @@
+﻿function Get-DBTClassTest
+{
+	[CmdletBinding()]
+    Param (
+        [Parameter(ValueFromPipeline = $true)]
+	    $InputObject
+	)
+	
+	begin
+	{
+		Write-PSFMessage -Level InternalComment -Message "Bound parameters: $($PSBoundParameters.Keys -join ", ")" -Tag 'debug','start','param'
+	}
+	process
+	{
+        foreach ($item in $Object) {
+            $properties = @()
+            $propertiesAssignment = @()
+            $methods = @()
+            
+            $frame = @"
+# Object of class to test
+`$item = New-Object $($item.GetType().FullName)
+
+#region Add in reasonable values into properties
+{0}
+#endregion Add in reasonable values into properties
+
+Describe "Testing the class $($item.GetType().FullName)" {{
+{1}
+}}
+"@
+            
+            $methodsToSkip = "ToString", "Equals", "GetHashCode", "GetType"
+            
+            foreach ($property in $item.PSOBject.Properties) {
+                $properties = @"
+    Context "Testing <$($item.GetType().Name)> Property $($property.Name)" {
+        It "Should be of type '$($property.TypeNameOfValue)'" {
+            `$item.PSOBject.Properties["$($property.Name)"].TypeNameOfValue | Should -Be "$($property.TypeNameOfValue)"
+        }
+        It "Should be safe to assign to" {
+            { `$item.$($property.Name) = 0 } | Should -Not -Throw
+        }
+        It "Should be safe to read from" {
+            { `$item.$($property.Name) } | Should -Not -Throw
+        }
+    }
+
+"@
+                $propertiesAssignment += "`$item.$($property.Name) = 0"
+                
+                $methodsToSkip += "get_($property.Name)"
+                $methodsToSkip += "set_($property.Name)"
+            }
+            
+            
+            foreach ($method in $item.PSObject.Methods) {
+                if ($methodsToSkip -contains $method.Name) { continue }
+            }
+        }
+    }
+    end
+	{
+	
+	}
+}
--- Daybreak2018Tools/internal/scripts/license.ps1
+++ Daybreak2018Tools/internal/scripts/license.ps1
@@ -1,5 +1,4 @@
-MIT License
-
+New-PSFLicense -Product 'Daybreak2018Tools' -Manufacturer 'PowerShell Summit 2018 Attendees' -ProductVersion $PSModuleVersion -ProductType Module -Name MIT -Version "1.0.0.0" -Date (Get-Date "2018-04-12") -Text @"
 Copyright (c) 2018 PowerShell Summit 2018 Attendees
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -19,3 +18,4 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+"@
