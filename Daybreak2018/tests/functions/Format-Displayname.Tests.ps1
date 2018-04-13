5a1195ec15d3166075111354f501f05d0434578c
JB Lewis <JB.Lewis@hennepin.us>
2018-04-12 18:15:18 -0700

    Eliminated unneeded tests

--- Daybreak2018/tests/functions/Format-Displayname.Tests.ps1
+++ Daybreak2018/tests/functions/Format-Displayname.Tests.ps1
@@ -1,15 +1,11 @@
 . .\internal\functions\Format-DisplayName.ps1
 
 Describe "Format-DisplayName" {
-            $a = "john"
+        $a = "john"
         $b = "lewis"
         $Results = Format-DisplayName -FirstName $a -Surname $b
-        $last, $first = $Results.split(" ")
-    it "will make the the last name uppercase" {
-        $last | Should be $b.ToUpper()
-    }
-    it "will make the first name Title case" {
-        $first | Should be (Get-Culture).TextInfo.ToTitleCase($FirstName)
-    }
 
+    it "outputs LAST First" {
+        $Results | Should Be "LEWIS John"
+    }
 }
